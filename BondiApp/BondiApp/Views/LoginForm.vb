026ec58a7d4cfe7df6a701c7a3b652a6f24b65e7
mihirchittora <mihirchittora6@gmail.com>
2018-04-17 18:58:47 +0530

    encrypted login

--- BondiApp/BondiApp/Views/LoginForm.vb
+++ BondiApp/BondiApp/Views/LoginForm.vb
@@ -1,4 +1,6 @@
-﻿Imports System.Text.RegularExpressions
+﻿Imports System.Security.Cryptography
+Imports System.Text
+Imports System.Text.RegularExpressions
 
 Public Class LoginForm
     Private m_utils As New Utils
@@ -17,13 +19,16 @@ Public Class LoginForm
         End If
         Try
             Using db As BondiModel = New BondiModel()
-                Dim loggedinUser = (From q In db.Memberships Where q.Email = txtUserId.Text AndAlso q.Password = txtPassword.Text Select q).FirstOrDefault()
+                'Dim userId = Guid.Parse(txtUserId.Text.Trim())
+                Dim loggedinUser = (From q In db.Memberships Where q.User.UserName = txtUserId.Text.Trim() Select q).FirstOrDefault()
                 If (loggedinUser IsNot Nothing) Then
-                    If (loggedinUser.IsApproved = True) Then
+                    Dim password = EncodePassword(txtPassword.Text.Trim(), loggedinUser.PasswordFormat, loggedinUser.PasswordSalt)
+
+                    If (password = loggedinUser.Password AndAlso loggedinUser.IsApproved = True) Then
                         Me.Hide()
                         mainForm.Show()
                     Else
-                        MsgBox("Your account is not approved so you cant login.")
+                        MsgBox("You are not authorised to enter the application")
                     End If
                 Else
                     MsgBox("Invalid UserId/Password")
@@ -37,39 +42,39 @@ Public Class LoginForm
     Dim EmailValid As Boolean = False
     Private Sub ValidateEmail()
 
-        Dim reEmail As Regex = New Regex("([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\." +
-        ")|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})",
-        RegexOptions.IgnoreCase _
-        Or RegexOptions.CultureInvariant _
-        Or RegexOptions.IgnorePatternWhitespace _
-        Or RegexOptions.Compiled
-        )
+        'Dim reEmail As Regex = New Regex("([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\." +
+        '")|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})",
+        'RegexOptions.IgnoreCase _
+        'Or RegexOptions.CultureInvariant _
+        'Or RegexOptions.IgnorePatternWhitespace _
+        'Or RegexOptions.Compiled
+        ')
 
-        Dim blnPossibleMatch As Boolean = reEmail.IsMatch(txtUserId.Text)
+        'Dim blnPossibleMatch As Boolean = reEmail.IsMatch(txtUserId.Text)
 
-        If blnPossibleMatch Then
+        'If blnPossibleMatch Then
 
-            If Not txtUserId.Text.Equals(reEmail.Match(txtUserId.Text).ToString) Then
+        '    If Not txtUserId.Text.Equals(reEmail.Match(txtUserId.Text).ToString) Then
 
-                MessageBox.Show("Invalid Email Address!")
+        '        MessageBox.Show("Invalid Email Address!")
 
-            Else
+        '    Else
 
-                EmailValid = True
+        '        EmailValid = True
 
-            End If
+        '    End If
 
-        Else
+        'Else
 
-            EmailValid = False
+        '    EmailValid = False
 
-            MessageBox.Show("Invalid Email Address!")
+        '    MessageBox.Show("Invalid Email Address!")
 
-            txtUserId.Clear()
+        '    txtUserId.Clear()
 
-            txtUserId.Focus()
+        '    txtUserId.Focus()
 
-        End If
+        'End If
 
     End Sub
 
@@ -83,7 +88,23 @@ Public Class LoginForm
 
     Private Sub LoginForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         txtUserId.Focus()
-        'Call m_utils.init(Me)
-        'mainForm.in
     End Sub
+
+
+    Private Function EncodePassword(ByVal pass As String, ByVal passwordFormat As Integer, ByVal salt As String) As String
+        If passwordFormat = 0 Then Return pass
+        Dim bIn As Byte() = Encoding.Unicode.GetBytes(pass)
+        Dim bSalt As Byte() = Convert.FromBase64String(salt)
+        Dim bAll As Byte() = New Byte(bSalt.Length + bIn.Length - 1) {}
+        Dim bRet As Byte() = Nothing
+        Buffer.BlockCopy(bSalt, 0, bAll, 0, bSalt.Length)
+        Buffer.BlockCopy(bIn, 0, bAll, bSalt.Length, bIn.Length)
+        If passwordFormat = 1 Then
+            Dim s As HashAlgorithm = HashAlgorithm.Create("SHA1")
+            bRet = s.ComputeHash(bAll)
+        End If
+
+        Return Convert.ToBase64String(bRet)
+    End Function
+
 End Class
